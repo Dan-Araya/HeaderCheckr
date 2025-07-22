1df33a7b4b3b3b198700421f0ac84c399f9dd841
Dan Araya <dan.araya.wk@gmail.com>
2025-07-22 18:49:06 -0400

    Add TODO in fetcher.py

--- backend/app/analyzer/fetcher.py
+++ backend/app/analyzer/fetcher.py
@@ -1,5 +1,6 @@
 import requests
 
 def get_headers(url: str) -> dict:
+    #TODO add User-Agent": "Mozilla/5.0 (compatible; HeaderCheckr/1.0)"
     response = requests.get(url, timeout=10)
     return dict(response.headers)
